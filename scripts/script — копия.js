dac7face4f19d6170600eafe1358b0a8dbd6c69b
Kotova Anna <merpri@mail.ru>
2022-04-10 15:09:51 +0300

    Проектная работа 5

--- scripts/script — копия.js
+++ scripts/script — копия.js
@@ -24,31 +24,26 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
-
 const containerElements = document.querySelector('.elements__group-elements');
 const template = document.querySelector('.template');
-
 const popup = document.querySelector('.popup');
-const popupForm = document.querySelector('.popup__form');
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close-btn');
+const closeButtonPopupAdded = document.querySelector('.popup__close-btn_place_popup__add-element');
 let inputName = document.querySelector('.popup__form-item_input_name');
 let inputAboutself = document.querySelector('.popup__form-item_input_aboutself');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
+const popupForm = document.querySelector('.popup__form');
+const buttonLikes = document.querySelectorAll('.element__button-like');
 
-const popupAddElement = document.querySelector('.popup__add-element');
-const popupFormAddElement = document.querySelector('.popup__form_add_element');
-const addButton = document.querySelector('.profile__add-button');
-const closeButtonPopupAdded = document.querySelector('.popup__close-btn_place_popup__add-element');
-let inputTitle = document.querySelector('.popup__form-item_input_title');
-let inputLinkPicture = document.querySelector('.popup__form-item_input_link-picture');
+function toggleActiveLikeButton (event) {
+  const element = event.target.closest(".element__button-like");
+  console.log(element);
+  element.classList.toggle('element__button-like_active');
+}
 
-const popupOpenPicture = document.querySelector('.popup__open-picture');
-const closeButtonPopupPicture = document.querySelector('.popup__close-btn_place_popup__open-picture');
-
-const likesButton = document.querySelectorAll('.element__button-like');
-const deleteButton = document.querySelector('.element__button-delete');
+containerElements.addEventListener('click', toggleActiveLikeButton);
 
 function render () {
   const elements = initialCards.map(createElement);
@@ -59,15 +54,11 @@ function createElement (item) {
   const newitem = template.content.cloneNode(true);
   const picture = newitem.querySelector('.element__place-img');
   const elementTitle = newitem.querySelector('.element__title');
-  const deleteButton = newitem.querySelector('.element__button-delete');
   const likeButton = newitem.querySelector('.element__button-like');
   picture.src = item.link;
   elementTitle.textContent = item.name;
-  deleteButton.addEventListener('click', deleteElement);
-  picture.addEventListener('click', openPopupOpenPicture);
   return newitem;
 }
-
 render ()
 
 function openedPopup() {
@@ -75,6 +66,12 @@ function openedPopup() {
   inputName.value = profileTitle.textContent;
   inputAboutself.value = profileSubtitle.textContent;
 }
+
+const addButton = document.querySelector('.profile__add-button');
+const popupAddElement = document.querySelector('.popup__add-element');
+const popupFormAddElement = document.querySelector('.popup__form_add_element')
+let inputTitle = document.querySelector('.popup__form-item_input_title');
+let inputLinkPicture = document.querySelector('.popup__form-item_input_link-picture');
 
 function openedPopupAddElement() {
   popupAddElement.classList.add('popup_opened');
@@ -100,7 +97,7 @@ function onSubmit1(event) {
   profileTitle.textContent = inputName.value;
   profileSubtitle.textContent = inputAboutself.value;
   closePopup();
-}
+};
 
 function onSubmit2(event) {
   event.preventDefault();
@@ -108,39 +105,29 @@ function onSubmit2(event) {
   closePopupAddElement();
 }
 
-function toggleActiveLikeButton (event) {
-  const element = event.target;
-  if (element.type === "button") {
-    element.classList.toggle('element__button-like_active');
-  }
-}
-
-function deleteElement(event) {
-  const element = event.target.closest('.element');
-  element.remove();
-}
-
-function openPopupOpenPicture (event) {
-  popupOpenPicture.classList.add('popup_opened');
-  const element = event.target.closest('.element');
-  const picture = element.querySelector('.element__place-img');
-  const elementTitle = element.querySelector('.element__title');
-  const popupPicture = popupOpenPicture.querySelector('.popup__picture');
-  const popupPictureTitle = popupOpenPicture.querySelector('.popup__title-picture');
-  popupPicture.src = picture.src;
-  popupPictureTitle.textContent = elementTitle.textContent;
-  console.log(element);
-}
-
-function closePopupOpenPicture () {
-  popupOpenPicture.classList.remove('popup_opened');
-}
-
-containerElements.addEventListener('click', toggleActiveLikeButton);
 popupForm.addEventListener('submit', onSubmit1);
 popupFormAddElement.addEventListener('submit', onSubmit2);
 editButton.addEventListener('click', openedPopup);
 addButton.addEventListener('click', openedPopupAddElement);
 closeButton.addEventListener('click', closePopup);
 closeButtonPopupAdded.addEventListener('click', closePopupAddElement);
-closeButtonPopupPicture.addEventListener('click', closePopupOpenPicture);
+/*
+let buttonLike = document.querySelectorAll('.elements__button-like')[0];
+let buttonLike1 = document.querySelectorAll('.elements__button-like')[1];
+let buttonLike2 = document.querySelectorAll('.elements__button-like')[2];
+let buttonLike3 = document.querySelectorAll('.elements__button-like')[3];
+let buttonLike4 = document.querySelectorAll('.elements__button-like')[4];
+let buttonLike5 = document.querySelectorAll('.elements__button-like')[5];
+
+function likeToggle() {
+  this.classList.toggle('elements__button-like_no-active');
+  this.classList.toggle('button_active');
+}
+
+buttonLike.addEventListener('click', likeToggle);
+buttonLike1.addEventListener('click', likeToggle);
+buttonLike2.addEventListener('click', likeToggle);
+buttonLike3.addEventListener('click', likeToggle);
+buttonLike4.addEventListener('click', likeToggle);
+buttonLike5.addEventListener('click', likeToggle);
+*/
